9be90f5a0444fca27d8cc27b10e47a2d3a885364
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-03-28 19:00:09 -0400

    return

--- assets/js/MainApplication.js
+++ assets/js/MainApplication.js
@@ -13,7 +13,7 @@ export class MainApplication {
   }
 
   async load() {
-    await this.fetchIpGeolocation('')
+   return await this.fetchIpGeolocation('')
   }
 
   async fetchIpGeolocation(ipContent) {
