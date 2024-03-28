952ff4f8d9ccd518b556c07b3119337445a05137
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-03-28 14:25:37 -0400

    reafatorando codigo

--- assets/js/MainApplication.js
+++ assets/js/MainApplication.js
@@ -8,27 +8,39 @@ export class MainApplication {
     this.locationInput = document.querySelector('.locationInput')
     this.timezoneInput = document.querySelector('.timezoneInput')
     this.ispInput = document.querySelector('.ispInput')
+    this.inputSearch = document.querySelector('.search-form input')
+    console.log(this.inputSearch);
+    this.btnInout = document.querySelector('.search-form button')
     this.map = new MapView()
   }
 
   async load() {
-    const request = new IpGeolocation();
-    try {
-      const { ip, country, region, city, lat, lng, timezone, isp } = await request.fetchAddress('');
-      this.ipAddressField.textContent = ip
-      this.locationInput.textContent = `${city}, ${region}`
-      this.timezoneInput.textContent = `UTC${timezone}`
-      this.ispInput.textContent = `${isp}`
-      this.map.loadMap(lat, lng)
+    await this.fetchIpGeolocation()
+  }
 
-      return { ip, country, region, city, lat, lng, timezone, isp };
+  async fetchIpGeolocation() {
+    try {
+      const { ip, country, region, city, lat, lng, timezone, isp } = await this.fetchAddressDetails('');
+      this.updateCards(ip, country, region, city, timezone, isp)
+      this.map.loadMap(lat, lng)
     } catch (error) {
       console.error('Error updating card:', error);
     }
   }
 
-  // getCardElements() {
+  async fetchAddressDetails() {
+    const request = new IpGeolocation()
+    try {
+      return await request.fetchAddress('')
+    } catch (e) {
+      alert('Algo está errado')
+    }
+  }
 
-  // }
-
+  updateCards(ip, city, region, timezone, isp) {
+    this.ipAddressField.textContent = ip
+    this.locationInput.textContent = `${city}, ${region}`
+    this.timezoneInput.textContent = `UTC${timezone}`
+    this.ispInput.textContent = `${isp}`
+  }
 }
